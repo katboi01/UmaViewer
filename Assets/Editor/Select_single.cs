e9100b3fe8b272c853d8fcd840ac1b97fd95c5e6
左右看 <1918891542@qq.com>
2022-09-27 13:41:05 +0800

    Some AssetBundle Support.

---
+++ Assets/Editor/Select_single.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+class SelectAllOfTagSingle : ScriptableWizard
+{
+
+    string tagName = "ExampleTag";
+
+
+    [MenuItem("Example/Get")]
+    static void SelectAllOfTagSingleWizard()
+    {
+        ScriptableWizard.DisplayWizard(
+            "Example/Get",
+            typeof(SelectAllOfTagSingle),
+            "Make Selection");
+    }
+
+    void OnWizardCreate()
+    {
+        AssetDatabase.RemoveObjectFromAsset(Selection.objects[0]);
+        AssetDatabase.CreateAsset(Selection.objects[0], "Assets/"+ Selection.objects[0].name + ".asset");
+    }
+}
